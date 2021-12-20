01f49dfd910fd8369cc260be9bbeeeb6d9965ea5
Afaq Ahmed <afaq.ahmed.retailo.co>
2021-12-20 11:41:14 +0500

    Final before Deployement

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
-// const BASE_URL = "http://ec2-34-244-97-51.eu-west-1.compute.amazonaws.com:3000";
+// const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://ec2-3-251-92-2.eu-west-1.compute.amazonaws.com:3000/";
 
 export const instance = axios.create({
   baseURL: BASE_URL,
